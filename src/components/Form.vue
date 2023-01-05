133060027825d1b46c1ba2aea8a5f3f8a69b023a
Mansur <vakhabov.mans@gmail.com>
2023-01-05 23:50:26 +0300

    реализовал удаление и добавление

---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <textarea
+      required
+      @click.prevent
+      type="text"
+      v-model="value"
+      placeholder="введите текст"
+    />
+    <button class="btnStn">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit("sendValue", this.value);
+      this.value = "";
+    },
+  },
+};
+</script>
---
+++ src/components/Notes.vue
@@ -0,0 +1,19 @@
+<template>
+  <div sendIndexForDelete="index" v-for="(note, index) in items" :key="index">
+    <div>
+      {{ note }}
+    </div>
+    <button @click="$emit('sendIndex', index)">🗑️</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
